237cf6dde5953f4cb8ecf56a02b3d52020c10493
Andrew Broman <broman@acm.org>
2016-04-06 21:44:26 -0500

    Config react-router and add Resource comp

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default (props) => {
+  return (
+    <div>
+      <h3>Resources</h3>
+      <ul>
+        <li>One Resource</li>
+        <li>Another Resource</li>
+        <li>Yet Another Resource</li>
+      </ul>
+    </div>
+  );
+}
