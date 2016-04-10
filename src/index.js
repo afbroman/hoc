69b294e47ee51a9ec3214c46ef933eec5522d2bd
Andrew Broman <broman@acm.org>
2016-04-09 23:12:21 -0500

    Configure for redux devtools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/app';
 import Resources from './components/resources';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = compose(
+  applyMiddleware(),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
